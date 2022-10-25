20d368efec20b0f6282caf3659066dfa15663c77
Dimitriy Goloborodko <31506838+mitya12342@users.noreply.github.com>
2022-10-26 00:12:30 +1000

    Create main.c

---
+++ main.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct veb_node veb;
+struct veb_node
+{
+    unsigned int u;
+    unsigned int min;
+    unsigned int max;
+    veb *summary;
+    veb **cluster;
+};
+
+const unsigned int NIL = -1;
+
+unsigned int veb_upper_sqrt(unsigned int u){
+    return pow(2, ceil(log2(u)/2));
+}
+
+unsigned int veb_lower_sqrt(unsigned int u){
+    // return pow(2, floor(log2(u)/2));
+    return 1U<<((int)log2(u) >> 1);
+}
+
+
+unsigned int veb_high(unsigned int u,  unsigned int x) {
+    return floor(x/veb_lower_sqrt(u));
+}
+
+unsigned int veb_low(unsigned int u, unsigned int x) {
+    return x % veb_lower_sqrt(u);
+}
+
+unsigned int veb_index(unsigned int u, unsigned int x, unsigned int y){
+    return x*veb_lower_sqrt(u)+y;
+}
+
+unsigned int veb_tree_minimum(veb* v) {
+    return v->min;
+}
+
+unsigned int veb_tree_maximum(veb* v) {
+    return v->max;
+}
+
+unsigned int veb_tree_member(veb* v, unsigned int x){
+    return
+        x == v->min || x == v->max ? 1 :
+        v->u == 2 ? 0 :
+        veb_tree_member(v->cluster[veb_high(v->u,x)], veb_low(v->u, x));
+}
+
+unsigned int veb_tree_successor(veb* v, unsigned int x){
+    if (v->u == 2)
+    {
+        if (x == 0 && v->max == 1) {
+            return 1;
+        } else {
+            return NIL;
+        }
+    } else if (v->min != NIL && x < v->min)
+    {
+        return v->min;
+    } else {
+        unsigned int max_low = veb_tree_maximum(v->cluster[veb_high(v->u,x)]);
+        if (max_low != NIL && veb_low(v->u, x) < max_low) {
+            unsigned int offset = veb_tree_successor(v->cluster[veb_high(v->u, x)], veb_low(v->u,x));
+            return veb_index(v->u, veb_high(v->u, x), offset);
+        }
+        else {
+            unsigned int succ_cluster = veb_tree_successor(v->summary, veb_high(v->u,x));
+            if (succ_cluster == NIL)
+            {
+                return NIL;
+            } else {
+                unsigned int offset = veb_tree_minimum(v->cluster[succ_cluster]);
+                return veb_index(v->u, succ_cluster, offset);
+            }
+        }
+    }
+}
+
+unsigned int veb_tree_predecessor(veb* v, unsigned int x){
+    if (v->u == 2) {
+        if (x == 1 && v->min == 0) {
+            return 0;
+        } else {
+            return NIL;
+        }
+    } else if (v->max != NIL && x > v->max)
+    {
+        return v->max;
+    } else {
+        unsigned int min_low = veb_tree_minimum(v->cluster[veb_high(v->u, x)]);
+        if (min_low != NIL && veb_low(v->u, x) > min_low) {
+            unsigned int offset = veb_tree_predecessor(v->cluster[veb_high(v->u, x)], veb_low(v->u, x));
+            return veb_index(v->u, veb_high(v->u, x), offset);
+        } else {
+            unsigned int pred_cluster = veb_tree_predecessor(v->summary, veb_high(v->u, x));
+            if (pred_cluster == NIL) {
+                if (v->min != NIL && x > v->min){
+                    return v->min;
+                } else {
+                    return NIL;
+                }
+            } else {
+                unsigned int offset = veb_tree_maximum(v->cluster[pred_cluster]);
+                return veb_index(v->u, pred_cluster, offset);
+            }
+        }
+    }
+}
+
+void veb_empty_tree_insert(veb* v, unsigned int x) {
+    v->min = x;
+    v->max = x;
+}
+
+void veb_exchange(unsigned int* a, unsigned int* b) {
+   unsigned int temp = *a;
+   *a = *b;
+   *b = temp;
+}
+
+void veb_tree_insert(veb* v, unsigned int x) {
+    if (v->min == NIL) {
+        veb_empty_tree_insert(v, x);
+    } else {
+        if (x < v->min) { 
+            veb_exchange(&x,&(v->min));
+        }
+        if (v->u > 2) {
+            if (veb_tree_minimum(v->cluster[veb_high(v->u,x)]) == NIL) {
+                veb_tree_insert(v->summary, veb_high(v->u, x));
+                veb_empty_tree_insert(v->cluster[veb_high(v->u, x)], veb_low(v->u, x));
+            } else {
+                veb_tree_insert(v->cluster[veb_high(v->u, x)], veb_low(v->u, x));
+            }
+        }
+        if (x > v->max)
+        {
+            v->max = x;
+        }
+        
+    }
+}
+
+veb* create_veb(unsigned int u) {
+    veb* new_veb = (veb*)calloc(1, sizeof(veb));
+    new_veb->max = NIL;
+    new_veb->min = NIL;
+    new_veb->u = u;
+    if (u > 2) {
+        unsigned int upper_sqrt = veb_upper_sqrt(u);
+        new_veb->summary = create_veb(upper_sqrt);
+        new_veb->cluster = calloc(upper_sqrt, sizeof(veb));
+        for (int i = 0; i < upper_sqrt; i++){
+            new_veb->cluster[i] = create_veb(upper_sqrt);
+        }
+    }
+    return new_veb;
+}
+
+void destroy_veb(veb* veb) {
+    if (veb->u > 2) {
+        unsigned int upper_sqrt = veb_upper_sqrt(veb->u);
+        destroy_veb(veb->summary);
+        for (int i = 0; i < upper_sqrt; i++) {
+            destroy_veb(veb->cluster[i]);
+        }
+        free(veb->cluster);
+    }
+    free(veb);
+}
+
+
+
+int main(int argc, char const *argv[])
+{
+    veb* root = create_veb(32);
+
+    veb_tree_insert(root, 5);
+    veb_tree_insert(root, 10);
+    veb_tree_insert(root, 3);
+    
+    printf("%u", veb_tree_member(root, 5)); 
+    printf("%u", veb_tree_member(root, 10)); 
+    printf("%u", !veb_tree_member(root, 15)); 
+
+    printf("%u", veb_tree_successor(root, 0) == 3);
+    printf("%u", veb_tree_successor(root, 3) == 5);
+    printf("%u", veb_tree_successor(root, 7) == 10);
+    printf("%u", veb_tree_successor(root, 10) == NIL);
+
+    destroy_veb(root);
+    return 0;
+}
+
